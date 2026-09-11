fcbd0724eebef9a2019478f86a7d5051faafa1aa
task-masterr01 <hemant.here96@gmail.com>
2026-09-11 12:16:40 +0530

    update firebase config with new app credentials

--- src/firebase.js
+++ src/firebase.js
@@ -8,7 +8,8 @@ const firebaseConfig = {
   projectId: "birthday-portfolio",
   storageBucket: "birthday-portfolio.firebasestorage.app",
   messagingSenderId: "968136223616",
-  appId: "1:968136223616:web:7a2f75b322008d56fd2f65",
+  appId: "1:968136223616:web:c5d9555d39bf1b14fd2f65",
+  measurementId: "G-6NXG5N7982"
 };
 
 const app      = initializeApp(firebaseConfig);
